44b1fb23a4089d597a412926857cd73e42b6dfe9
hellenilda <hellen95limaaraujo@gmail.com>
2025-09-30 11:09:33 -0300

    :sparkles: feat: adicionar novo produto

---
+++ src/components/create-modal/create-modal.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import "./modal.css"
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate"
+
+const Input = ({ label, value, updateValue }) => {
+    return (
+        <>
+            <label >{label}</label>
+            <input value={value} onChange={event => updateValue(event.target.value)} />
+        </>
+    )
+}
+
+export function CreateModal({ isOpen, setModalOpen }) {
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState(0)
+    const [image, setImage] = useState("")
+    const { mutate, isSuccess, isLoading } = useFoodDataMutate()
+
+    const submit = () => {
+        const foodData = {
+            title,
+            price,
+            image
+        }
+
+        mutate(foodData)
+    }
+
+    useEffect(() => {
+        if(isSuccess) {
+            setModalOpen(false)
+        }
+    }, [isSuccess, setModalOpen])
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-body">
+                <h2>Cadastre um novo item no cardápio</h2>
+                <form className="input-container">
+                    <Input label="Título" value={title} updateValue={setTitle} />
+                    <Input label="Preço" value={price} updateValue={setPrice} />
+                    <Input label="Imagem" value={image} updateValue={setImage} />
+                </form>
+                <button onClick={submit} className="btn-secondary">
+                    {isLoading ? 'Postando...' : 'Postar'}
+                </button>
+                <button onClick={() => setModalOpen(false)} className="btn-primary">Cancelar</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/hooks/useFoodDataMutate.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+const API_URL = '/api'
+
+const postData = async (data) => {
+    const response = await axios.post(API_URL + '/food', data)
+    return response.data
+}
+
+export function useFoodDataMutate() {
+    const queryClient = useQueryClient()
+    
+    const mutate = useMutation({
+        mutationFn: postData,
+        retry: 2,
+        onSuccess: () => {
+            queryClient.invalidateQueries(['food-data'])
+        }
+    })
+
+    return mutate
+}
